8a3eaf4005fc6051abcbd132c2da285d6296c684
Ronnie Matijsen <ronnie@brainstormenconcept.nl>
2014-05-02 16:52:10 +0200

    bf

--- js/index.js
+++ js/index.js
@@ -196,58 +196,43 @@ function onPushwooshAndroidInitialized(pushToken)
 	}
 }
 
-function downloadFile() {
-    console.log('downloadFile');
-    window.requestFileSystem(
-        LocalFileSystem.PERSISTENT,
-        0,
-        onRequestFileSystemSuccess,
-        fail
-    );
+function storeBrochure(){
+  
+  //var url = "http://www.evabydorc.com/app/cms/app/webroot/assets/brochures/27g/(1).jpg"; // image url
+  window.requestFileSystem(
+                     LocalFileSystem.PERSISTENT, 0, 
+                     function onFileSystemSuccess(fileSystem) {
+                     fileSystem.root.getFile(
+                                 "dummy.html", {create: true, exclusive: false}, 
+                                 function gotFileEntry(fileEntry){
+                                 var sPath = fileEntry.fullPath.replace("dummy.html","");
+                                 var fileTransfer = new FileTransfer();
+                                 fileEntry.remove();
+ 
+                                 fileTransfer.download(
+                                           "http://www.evabydorc.com/app/cms/app/webroot/assets/brochures/27g/(1).jpg",
+                                           fileSystem.root.toURL() + "nani.jpg",
+                                           function(theFile) {
+                                           console.log("download complete: " + theFile.toURL());
+                                           showLink(theFile.toURL());
+                                           },
+                                           function(error) {
+                                           console.log("download error source " + error.source);
+                                           console.log("download error target " + error.target);
+                                           console.log("upload error code: " + error.code);
+                                           }
+                                           );
+                                 }, 
+                                 fail);
+                     }, 
+                     fail);
 }
 
-function onRequestFileSystemSuccess(fileSystem) {
-    console.log('onRequestFileSystemSuccess');
-    fileSystem.root.getFile(
-        'dummy.html',
-        {create: true, exclusive: false},
-        onGetFileSuccess,
-        fail
-    );
-}
-
-function onGetFileSuccess(fileEntry) {
-    console.log('onGetFileSuccess!');
-    var path = fileEntry.toURL().replace('dummy.html', '');
-    console.log(path);
-    var fileTransfer = new FileTransfer();
-    fileEntry.remove();
-    console.log(fileTransfer);
-    fileTransfer.download(
-        'http://www.evabydorc.com/app/cms/app/webroot/assets/brochures/27g/(1).jpg',
-        fileSystem.root.toURL() + '/' + 'theFile.jpg',
-        function(file) {
-            console.log('download complete: ' + file.toURI());
-            showLink(file.toURI());
-        },
-        function(error) {
-            console.log('download error source ' + error.source);
-            console.log('download error target ' + error.target);
-            console.log('upload error code: ' + error.code);
-        }
-    );
-}
-
-function showLink(url) {
+function showLink(url){
     alert(url);
-    var divEl = document.getElementById('bericht');
-    var aElem = document.createElement('a');
-    aElem.setAttribute('target', '_blank');
-   aElem.setAttribute('href', url);
-   aElem.appendChild(document.createTextNode('Ready! Click To Open.'));
-   divEl.appendChild(aElem);
-}
-
+ 
+    }
+    
 function fail(evt) {
     console.log(evt.target.error.code);
 }
