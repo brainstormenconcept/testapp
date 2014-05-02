cbe4463b9b729ddfcb6ff7ac0a8cd262b5d255bb
Ronnie Matijsen <ronnie@brainstormenconcept.nl>
2014-05-02 15:54:16 +0200

    nu

--- js/index.js
+++ js/index.js
@@ -199,26 +199,42 @@ function onPushwooshAndroidInitialized(pushToken)
 function storeBrochure(){
   
   //var url = "http://www.evabydorc.com/app/cms/app/webroot/assets/brochures/27g/(1).jpg"; // image url
-  window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function onFileSystemSuccess(fileSystem) {
-		fileSystem.root.getFile("dummy.html", { create: true, exclusive: false }, function gotFileEntry(fileEntry) {
-			var sPath = fileEntry.toURL().replace("dummy.html", "");
-			var fileTransfer = new FileTransfer();
-			fileEntry.remove();
-			var DBuri = encodeURI("http://www.evabydorc.com/app/cms/app/webroot/assets/brochures/27g/(1).jpg");
-			fileTransfer.download(DBuri, sPath + "nani.png", function (theFile) {
-    			console.log("download complete: " + theFile.toURI());
-    			//showLink(theFile.toURI());
-    			//setTimeout(function () {
-        		//	checkConnection();
-    			//}, 50);
-			},
-			function (error) {
-			    console.log("download error source " + error.source);
-			    console.log("download error target " + error.target);
-			    console.log("upload error code: " + error.code);
-			});
-		}, fail);
-	}, fail);
+  window.requestFileSystem(
+                     LocalFileSystem.PERSISTENT, 0, 
+                     function onFileSystemSuccess(fileSystem) {
+                     fileSystem.root.getFile(
+                                 "dummy.html", {create: true, exclusive: false}, 
+                                 function gotFileEntry(fileEntry){
+                                 var sPath = fileEntry.fullPath.replace("dummy.html","");
+                                 var fileTransfer = new FileTransfer();
+                                 fileEntry.remove();
+ 
+                                 fileTransfer.download(
+                                           "http://www.w3.org/2011/web-apps-ws/papers/Nitobi.pdf",
+                                           sPath + "theFile.pdf",
+                                           function(theFile) {
+                                           console.log("download complete: " + theFile.toURI());
+                                           showLink(theFile.toURI());
+                                           },
+                                           function(error) {
+                                           console.log("download error source " + error.source);
+                                           console.log("download error target " + error.target);
+                                           console.log("upload error code: " + error.code);
+                                           }
+                                           );
+                                 }, 
+                                 fail);
+                     }, 
+                     fail);
+}
+
+function showLink(url){
+    alert(url);
+ 
+    }
+    
+function fail(evt) {
+    console.log(evt.target.error.code);
 }
 
 var app = {
