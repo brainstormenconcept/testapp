3f3ffd29d245d755cface931743c30b8987bb6b2
Ronnie Matijsen <ronnie@brainstormenconcept.nl>
2014-05-02 12:11:58 +0200

    pw

--- js/index.js
+++ js/index.js
@@ -15,7 +15,139 @@
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
+ * 
+ * 
  */
+
+function registerPushwooshAndroid() {
+
+ 	var pushNotification = window.plugins.pushNotification;
+
+	//push notifications handler
+	document.addEventListener('push-notification', function(event) {
+	            var title = event.notification.title;
+	            var userData = event.notification.userdata;
+
+	            //dump custom data to the console if it exists
+	            if(typeof(userData) != "undefined") {
+					console.warn('user data: ' + JSON.stringify(userData));
+				}
+
+				//and show alert
+				navigator.notification.alert(title);
+
+				//stopping geopushes
+				pushNotification.stopGeoPushes();
+			  });
+
+	//projectid: "GOOGLE_PROJECT_ID", appid : "PUSHWOOSH_APP_ID"
+	pushNotification.registerDevice({ projectid: "821107598432", appid : "2A963-861C4" },
+									function(token) {
+										alert(token);
+										//callback when pushwoosh is ready
+										onPushwooshAndroidInitialized(token);
+									},
+									function(status) {
+										alert("failed to register: " +  status);
+									    console.warn(JSON.stringify(['failed to register ', status]));
+									});
+ }
+
+function onPushwooshAndroidInitialized(pushToken)
+{
+	//output the token to the console
+	console.warn('push token: ' + pushToken);
+
+	var pushNotification = window.plugins.pushNotification;
+	
+	pushNotification.getTags(function(tags) {
+							console.warn('tags for the device: ' + JSON.stringify(tags));
+						 },
+						 function(error) {
+							console.warn('get tags error: ' + JSON.stringify(error));
+						 });
+	 
+
+	//set multi notificaiton mode
+	//pushNotification.setMultiNotificationMode();
+	//pushNotification.setEnableLED(true);
+	
+	//set single notification mode
+	//pushNotification.setSingleNotificationMode();
+	
+	//disable sound and vibration
+	//pushNotification.setSoundType(1);
+	//pushNotification.setVibrateType(1);
+	
+	pushNotification.setLightScreenOnNotification(false);
+	
+	//goal with count
+	//pushNotification.sendGoalAchieved({goal:'purchase', count:3});
+	
+	//goal with no count
+	//pushNotification.sendGoalAchieved({goal:'registration'});
+
+	//setting list tags
+	//pushNotification.setTags({"MyTag":["hello", "world"]});
+	
+	//settings tags
+	pushNotification.setTags({deviceName:"hello", deviceId:10},
+									function(status) {
+										console.warn('setTags success');
+									},
+									function(status) {
+										console.warn('setTags failed');
+									});
+		
+	function geolocationSuccess(position) {
+		pushNotification.sendLocation({lat:position.coords.latitude, lon:position.coords.longitude},
+								 function(status) {
+									  console.warn('sendLocation success');
+								 },
+								 function(status) {
+									  console.warn('sendLocation failed');
+								 });
+	};
+		
+	// onError Callback receives a PositionError object
+	//
+	function geolocationError(error) {
+		alert('code: '    + error.code    + '\n' +
+			  'message: ' + error.message + '\n');
+	}
+	
+	function getCurrentPosition() {
+		navigator.geolocation.getCurrentPosition(geolocationSuccess, geolocationError);
+	}
+	
+	//greedy method to get user position every 3 second. works well for demo.
+//	setInterval(getCurrentPosition, 3000);
+		
+	//this method just gives the position once
+//	navigator.geolocation.getCurrentPosition(geolocationSuccess, geolocationError);
+		
+	//this method should track the user position as per Phonegap docs.
+//	navigator.geolocation.watchPosition(geolocationSuccess, geolocationError, { maximumAge: 3000, enableHighAccuracy: true });
+
+	//Pushwoosh Android specific method that cares for the battery
+	pushNotification.startGeoPushes();
+}
+
+ function initPushwoosh() {
+	var pushNotification = window.plugins.pushNotification;
+	if(device.platform == "Android")
+	{
+		registerPushwooshAndroid();
+		pushNotification.onDeviceReady();
+	}
+
+	if(device.platform == "iPhone" || device.platform == "iOS")
+	{
+		registerPushwooshIOS();
+		pushNotification.onDeviceReady();
+	}
+}
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -35,7 +167,7 @@ var app = {
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
-        app.initPushwoosh();
+        initPushwoosh();
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
@@ -47,35 +179,5 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
-    },
-    
-    initPushwoosh: function(){
-	    var pushNotification = window.plugins.pushNotification;
-	    pushNotification.onDeviceReady();
-	 	console.log(1);
-	    pushNotification.registerDevice({ projectid: "821107598432", appid : "2A963-861C4" },
-	        function(status) {
-	            var pushToken = status;
-	            console.warn('push token: ' + pushToken);
-	            console.log(status);
-	        },
-	        function(status) {
-	            console.warn(JSON.stringify(['failed to register ', status]));
-	            console.log(status);
-	        }
-	        
-	    );
- 		console.log('Received Event: hoi');
-	    document.addEventListener('push-notification', function(event) {
-	        var title = event.notification.title;
-	            var userData = event.notification.userdata;
-	                                 
-	            if(typeof(userData) != "undefined") {
-	            console.warn('user data: ' + JSON.stringify(userData));
-	        }
-	                                     
-	        navigator.notification.alert(title);
-	    });
-	    
-	}
+    }
 };
