d57b14ee33209f97ca1e086fa52b245c105bf6e6
Ronnie Matijsen <ronnie@brainstormenconcept.nl>
2014-05-02 16:02:56 +0200

    fgh

--- js/index.js
+++ js/index.js
@@ -196,43 +196,57 @@ function onPushwooshAndroidInitialized(pushToken)
 	}
 }
 
-function storeBrochure(){
-  
-  //var url = "http://www.evabydorc.com/app/cms/app/webroot/assets/brochures/27g/(1).jpg"; // image url
-  window.requestFileSystem(
-                     LocalFileSystem.PERSISTENT, 0, 
-                     function onFileSystemSuccess(fileSystem) {
-                     fileSystem.root.getFile(
-                                 "dummy.html", {create: true, exclusive: false}, 
-                                 function gotFileEntry(fileEntry){
-                                 var sPath = fileEntry.fullPath.replace("dummy.html","");
-                                 var fileTransfer = new FileTransfer();
-                                 fileEntry.remove();
- 
-                                 fileTransfer.download(
-                                           "http://www.w3.org/2011/web-apps-ws/papers/Nitobi.pdf",
-                                           sPath + "theFile.pdf",
-                                           function(theFile) {
-                                           console.log("download complete: " + theFile.toURI());
-                                           showLink(theFile.toURI());
-                                           },
-                                           function(error) {
-                                           console.log("download error source " + error.source);
-                                           console.log("download error target " + error.target);
-                                           console.log("upload error code: " + error.code);
-                                           }
-                                           );
-                                 }, 
-                                 fail);
-                     }, 
-                     fail);
+function downloadFile() {
+    console.log('downloadFile');
+    window.requestFileSystem(
+        LocalFileSystem.PERSISTENT,
+        0,
+        onRequestFileSystemSuccess,
+        fail
+    );
 }
 
-function showLink(url){
-    alert(url);
- 
-    }
+function onRequestFileSystemSuccess(fileSystem) {
+    console.log('onRequestFileSystemSuccess');
+    fileSystem.root.getFile(
+        'dummy.html',
+        {create: true, exclusive: false},
+        onGetFileSuccess,
+        fail
+    );
+}
+
+function onGetFileSuccess(fileEntry) {
+    console.log('onGetFileSuccess!');
+    var path = fileEntry.toURL().replace('dummy.html', '');
+    var fileTransfer = new FileTransfer();
+    fileEntry.remove();
     
+    fileTransfer.download(
+        'http://www.w3.org/2011/web-apps-ws/papers/Nitobi.pdf',
+        path + 'theFile.pdf',
+        function(file) {
+            console.log('download complete: ' + file.toURI());
+            showLink(file.toURI());
+        },
+        function(error) {
+            console.log('download error source ' + error.source);
+            console.log('download error target ' + error.target);
+            console.log('upload error code: ' + error.code);
+        }
+    );
+}
+
+function showLink(url) {
+    alert(url);
+    //var divEl = document.getElementById('deviceready');
+    //var aElem = document.createElement('a');
+    //aElem.setAttribute('target', '_blank');
+   // aElem.setAttribute('href', url);
+   // aElem.appendChild(document.createTextNode('Ready! Click To Open.'));
+   // divEl.appendChild(aElem);
+}
+
 function fail(evt) {
     console.log(evt.target.error.code);
 }
